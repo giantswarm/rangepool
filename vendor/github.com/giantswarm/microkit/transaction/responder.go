54276bd234cdb330185c72092bfca812aca8d31c
Paweł Kopiczko <kopiczko@gmail.com>
2017-09-14 16:30:57 +0200

    use microstorage K/KV interface

--- vendor/github.com/giantswarm/microkit/transaction/responder.go
+++ vendor/github.com/giantswarm/microkit/transaction/responder.go
@@ -7,47 +7,24 @@ import (
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-
-	microstorage "github.com/giantswarm/microkit/storage"
+	"github.com/giantswarm/microstorage"
 )
 
 // ResponderConfig represents the configuration used to create a responder.
 type ResponderConfig struct {
 	// Dependencies.
 	Logger  micrologger.Logger
-	Storage microstorage.Service
+	Storage microstorage.Storage
 }
 
 // DefaultResponderConfig provides a default configuration to create a new
 // responder by best effort.
 func DefaultResponderConfig() ResponderConfig {
-	var err error
-
-	var loggerService micrologger.Logger
-	{
-		loggerConfig := micrologger.DefaultConfig()
-		loggerService, err = micrologger.New(loggerConfig)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	var storageService microstorage.Service
-	{
-		storageConfig := microstorage.DefaultConfig()
-		storageService, err = microstorage.New(storageConfig)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	config := ResponderConfig{
+	return ResponderConfig{
 		// Dependencies.
-		Logger:  loggerService,
-		Storage: storageService,
+		Logger:  nil,
+		Storage: nil,
 	}
-
-	return config
 }
 
 // NewResponder creates a new configured responder.
@@ -72,11 +49,14 @@ func NewResponder(config ResponderConfig) (Responder, error) {
 type responder struct {
 	// Dependencies.
 	logger  micrologger.Logger
-	storage microstorage.Service
+	storage microstorage.Storage
 }
 
 func (r *responder) Exists(ctx context.Context, transactionID string) (bool, error) {
-	key := responseKey("transaction", transactionID)
+	key, err := microstorage.NewK(responseKey("transaction", transactionID))
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
 	exists, err := r.storage.Exists(ctx, key)
 	if err != nil {
 		return false, microerror.Mask(err)
@@ -91,7 +71,10 @@ func (r *responder) Reply(ctx context.Context, transactionID string, rr Response
 	// cannot find the desired transaction response, we return an error.
 	var response Response
 	{
-		key := responseKey("transaction", transactionID)
+		key, err := microstorage.NewK(responseKey("transaction", transactionID))
+		if err != nil {
+			return microerror.Mask(err)
+		}
 		res, err := r.storage.Search(ctx, key)
 		if microstorage.IsNotFound(err) {
 			return microerror.Mask(notFoundError)
@@ -99,7 +82,7 @@ func (r *responder) Reply(ctx context.Context, transactionID string, rr Response
 			return microerror.Mask(err)
 		}
 
-		err = json.Unmarshal([]byte(res), &response)
+		err = json.Unmarshal([]byte(res.Val()), &response)
 		if err != nil {
 			return microerror.Mask(err)
 		}
@@ -163,7 +146,11 @@ func (r *responder) Track(ctx context.Context, transactionID string, rt Response
 	// Now we have all information in place to store the transaction response.
 	{
 		key := responseKey("transaction", transactionID)
-		err := r.storage.Create(ctx, key, val)
+		kv, err := microstorage.NewKV(key, val)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		err = r.storage.Put(ctx, kv)
 		if err != nil {
 			return microerror.Mask(err)
 		}
